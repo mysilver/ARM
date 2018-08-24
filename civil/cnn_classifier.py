fe380772a4ffc1bf20342a51d70c0809b7024243
MAY <m.yaghubzade@gmail.com>
2018-08-24 23:36:05 +1000

    Test & Train

--- civil/cnn_classifier.py
+++ civil/cnn_classifier.py
@@ -1,34 +1,52 @@
 import tensorflow as tf
 from keras import Sequential
 from keras.layers import Dense, Conv1D, MaxPool1D, Flatten
-from keras.utils.vis_utils import plot_model
+# from keras.utils.vis_utils import plot_model
+from sklearn.cross_validation import StratifiedKFold
+from civil.utils import convert_matlab_file, load_pickle
 
-from civil.utils import convert_matlab_file, load_dataset
+
+def create_model():
+    model = Sequential()
+    print("Sensor Shape (after reduction):", X_train.shape)
+    model.add(Conv1D(filters=20, kernel_size=2, input_shape=(signal_length, number_of_sensors)))
+    model.add(MaxPool1D())
+    model.add(Conv1D(filters=20, kernel_size=2, input_shape=(signal_length, number_of_sensors)))
+    model.add(MaxPool1D())
+    model.add(Flatten())
+    model.add(Dense(number_of_classes, activation="sigmoid"))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
+    # plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
+    model.summary()
+    return model
+
 
 if __name__ == "__main__":
 
+    number_of_samples = 240
+    number_of_samples_per_class = 40
+    number_of_classes = number_of_samples / number_of_samples_per_class
+    signal_length = 5001  # the length of signal
+    number_of_sensors = 28
+    epochs = 500
+    batch_size = 64
+    path = "civil.pickle"
+
     try:
         print("Loading the data")
-        path = "civil.pickle"
-        X_train, Y_train = load_dataset(path)
+        X_train, Y_train = load_pickle(path)
         print(X_train.shape, "==>", Y_train.shape)
     except:
         print("Converting Matlab file to python compatible file")
-        X_train, Y_train = convert_matlab_file(240, 'Bridge_01.mat', path)
+        X_train, Y_train = convert_matlab_file(number_of_samples, number_of_samples_per_class, signal_length,
+                                               number_of_sensors, 'Bridge_01.mat', path)
 
-    with tf.device("/gpu:1"):
-        sensor_size = 5001
-        # sensor_size, X_train = reduce_last_dimension(X_train, sensor_size)
-        model = Sequential()
-        print("Sensor Shape (after reduction):", X_train.shape)
-        model.add(Conv1D(filters=20, kernel_size=2, input_shape=(sensor_size, 28)))
-        model.add(MaxPool1D())
-        model.add(Conv1D(filters=20, kernel_size=2, input_shape=(sensor_size, 28)))
-        model.add(MaxPool1D())
-        model.add(Flatten())
-        model.add(Dense(6, activation="sigmoid"))
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=["accuracy"])
-        plot_model(model, to_file='model_plot.png', show_shapes=True, show_layer_names=True)
-        model.summary()
-
-        model.fit(X_train, Y_train, epochs=500, batch_size=64)
+    # sensor_size, X_train = reduce_last_dimension(X_train, sensor_size)
+    with tf.device("/cpu:0"):
+        print("Traning is started")
+        kfold = StratifiedKFold(Y_train, n_folds=5)
+        for train, test in kfold:
+            model = create_model()
+            model.fit(X_train[train], Y_train[train], epochs=epochs, batch_size=batch_size)
+            scores = model.evaluate(X_train[test], Y_train[test], verbose=1)
+            print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
--- civil/utils.py
+++ civil/utils.py
@@ -11,20 +11,20 @@ def shuffle(a, b):
     return a[p], b[p]
 
 
-def convert_matlab_file(samples, matlab_file, save_path):
+def convert_matlab_file(samples, number_of_samples_per_class,signal_length, number_of_sensors, matlab_file, save_path):
     x = scipy.io.loadmat(matlab_file)
-    sensors = np.transpose(x['InputData'], (3, 0, 1, 2))
-    print(sensors.shape)
-    sensors = sensors.reshape(samples, 5001, 28)
 
+    sensors = np.transpose(x['InputData'], (3, 0, 1, 2)) # reorder the file
+    print(sensors.shape)
+    sensors = sensors.reshape(samples, signal_length, number_of_sensors)
     X_train = sensors
-    Y_train = np.array([math.ceil(i / 40) -1 for i in range(1, 241)])
+    Y_train = np.array([math.ceil(i / number_of_samples_per_class) -1 for i in range(1, 241)])
     Y_train = np_utils.to_categorical(Y_train)
     X_train, Y_train = shuffle(X_train, Y_train)
     np.savez(save_path, X_train, Y_train)
     return X_train, Y_train
 
 
-def load_dataset(path):
+def load_pickle(path):
     t = np.load(path + ".npz")
     return t['arr_0'], t['arr_1']
